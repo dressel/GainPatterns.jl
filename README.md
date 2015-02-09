9b65a93c9c30fd9f874b248ee37e86c388a6625e
Louis Dressel <dressel@stanford.edu>
2015-02-09 11:14:47 -0800

    Added the beginnings of functionality. More to come

--- README.md
+++ README.md
@@ -2,7 +2,26 @@
 
 The purpose of this package is to allow the plotting of gain patterns. It is assumed that you have the gain pattern in some text file, and wish to plot it. Some basic gain pattern manipulation (like cross-correlation with similar patterns) will be allowed. It would be cool if gain patterns could be generated from simple antennas, but that is not really what this is for.
 
-For now, I will simply plot using PGFPlots, but in the future, I might add PyPlot and allow the user to choose.
+## Available Functions
+`normalize(gains::Vector{Float64})` returns a new vector with normalized gains.
 
-This came default with the package when I made it. Not sure what to do with it.
+`normalize!(gains::Vector{Float64})` normalizes the provided vector of gains. Similar to `normalize`, but more memory conscious.
+
+`crosscorrelate(ref, sample, angles)` performs a cross-correlation of the sample and a reference signal. The idea is that the reference is of length 360, and the sample can be of variable length. `angles` is a vector of the angles at which `sample` was sampled.
+
+## Near-Term Plans
+This is currently still very rough. Some things I want to add:
+* Better/more documentation
+* Plotting ability
+* Maybe create a GainPattern type?
+* Allow creation of said type through a text or csv file
+
+## Future Plans
+Future plans (like way down the road):
+* Allow the use of PyPlot as well as PGFPlots
+* Read in an image of a gain pattern and spit out the resulting values (a bit quixotic, but I suppose it is doable).
+* Generate gain patterns for simple antennas. I do not currently have the expertise to make that happen.
+
+## Build Status
+This came default with the package when I made it. Not sure what to do with it or what it does.
 [![Build Status](https://travis-ci.org/dressel/GainPatterns.jl.svg?branch=master)](https://travis-ci.org/dressel/GainPatterns.jl)
--- src/GainPatterns.jl
+++ src/GainPatterns.jl
@@ -1,10 +1,73 @@
 module GainPatterns
 
 # package code goes here
-export testgains
+export normalize, normalize!, sampleGains, crosscorrelate
 
-function testgains()
-  println("hello!")
+# Returns a normalized distribution
+# This normalization was shown in Graefenstein 2009
+function normalize(gains::Vector{Float64})
+	newGains = Array(Float64, size(gains))
+	return normalize!(gains)
+end
+
+# In place version of the above.
+function normalize!(gains::Vector{Float64})
+	m = mean(gains)
+	s = std(gains)
+	l = length(gains)
+	for i = 1:l
+		gains[i] = (gains[i] - m) / s
+	end
+	return gains
+end
+
+# Sample from a given distribution of gains
+# Returns the gains sampled, and the angles at which these samples occurred
+function sampleGains(gains, step::Int64, bearing::Int64)
+
+	noise = 0.25*randn(360)
+
+	# antenna_angles are angles antenna is oriented at
+	# sampled_angles is angle antenna observes in its measurements
+	antenna_angles = [0:step:359]
+	sampled_angles = mod(antenna_angles - bearing, 360)
+	sampled_gains = gains[sampled_angles + 1] + noise[sampled_angles + 1]
+	normalize!(sampled_gains)
+	return sampled_gains, antenna_angles
+end
+
+# Function carries out cross-correlation to determine the bearing
+# angles is the vector of angles corresponding to the samples
+#
+# Returns vector containing shifts at every degree
+#	ith index corresponds to (i-1) shift
+# TODO: Make this more efficient
+#
+# The idea is that the reference is of length 360, 
+#  but the sample need not be.
+# Therefore, a vector of sampled angles is provided.
+function crosscorrelate(ref::Vector{Float64}, sample::Vector{Float64}, angles)
+	shifts = [0:359]
+	cs = zeros(length(shifts))
+	for shift in shifts
+		cs[shift+1] = cc_helper(shift, ref, sample, angles)
+	end
+	return cs
+end
+
+
+# Helper function to perform cross-correlation
+# This is the cross-correlation function used in Graefenstein 2009
+# Inputs are:
+#	rarkk
+function cc_helper(shift, ref, sample, angles)
+	i = 1
+	c = 0
+	for deg in angles
+		c += sample[i] * ref[mod(deg - shift, 360) + 1]
+		i += 1
+	end
+	return c
 end
 
 end # module
