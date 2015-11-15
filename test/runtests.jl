19dc08b6d6474531e3b6cede4f8b8732b14d494b
Louis Dressel <dressel@stanford.edu>
2015-11-15 12:49:43 -0800

    added some tests for angular error

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using GainPatterns
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+#################################################################
+# testing angular_error function
+#################################################################
+@test angular_error(5, 355) == 10
+@test angular_error(355, 5) == 10
+
+@test angular_error(5, -5) == 10
+@test angular_error(-5, 5) == 10
+
+@test angular_error(340, 17) == 37
+@test angular_error(17, 340) == 37
+
+@test angular_error(-17, 340) == 3
+@test angular_error(340, -17) == 3
